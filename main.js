3e541e20f70d6dbec849ff7a10eaa5031784100f
Matt Rowan <matt.rowan121@gmail.com>
2022-11-01 17:25:16 -0400

    feat: Add variables and query selectors

---
+++ main.js
@@ -0,0 +1,16 @@
+/* ~~~ VARIABLES/QUERY SELECTOR ~~~ */ 
+
+var allIdeas = [];
+var showIdeasButton = document.querySelector('.show-ideas-button');
+var ideaBox = document.querySelector('.container-top')
+var userTitle = document.querySelector('.user-title');
+var userBody = document.querySelector('.user-body');
+var saveIdeaButton = document.querySelector('.save-button');
+var searchImage = document.querySelector('.search-image');
+var searchField = document.querySelector('.search-field');
+var ideaCard = document.querySelector('.idea-card');
+var ideaContent = document.querySelector('.idea-content');
+
+/* ~~~ EVENT HANDLERS ~~~ */ 
+
+/* ~~~ FUNCTIONS ~~~ */ 
